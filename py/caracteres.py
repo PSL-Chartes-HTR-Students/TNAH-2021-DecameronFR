32c35a569fa838aa3cd5cf0040e860175749b0a4
Sebastien Biay <sbiayha@outlook.fr>
2021-11-19 14:48:55 +0100

    Caracteres : regroupement des types de caractères dans des articles (lettres, ponctuation, etc.)

--- py/caracteres.py
+++ py/caracteres.py
@@ -4,7 +4,15 @@ with open("csv/caracteres.csv") as f:
     lecteur = csv.reader(f, delimiter="\t", quotechar='|')
 
     donnees = {}
-    
+    ttTypes = [
+        'lettre',
+        'ponctuation',
+        'signe combiné',
+        'abréviation',
+        'signe tironien',
+        'exemple'
+    ]
+
     for adresse_img, typ, lettre, majmin, commentaire, probleme, encodage in lecteur:
         donnees[adresse_img] = {
             "type" : typ,
@@ -14,7 +22,7 @@ with open("csv/caracteres.csv") as f:
             "problème" : probleme,
             "encodage" : encodage}
     
-with open("../caracteres.html", mode="w") as g:
+with open("caracteres.html", mode="w") as g:
     g.write("<!DOCTYPE html>\n<html>\n")
     # Ecriture du head
     g.write("<head>\n")
@@ -22,34 +30,46 @@ with open("../caracteres.html", mode="w") as g:
     g.write('''<link type="text/css" rel="stylesheet" href="css/caracteres.css">\n''')
     g.write("</head>\n")
     
-    
     # Ecriture du body
     g.write("<body>\n")
-    for index, item in enumerate(donnees):
-        if index != 0:
-            # Ecriture des articles
-            g.write("<article>\n")
-            
-            # Div contenant les images
-            g.write(f'''<div class="img"><img src="{item}" alt="{donnees[item]["caractere"]}" height="100"></div>\n''')
-            
-            # Div contenant le contenu des notices
-            g.write(f'''<div class="descript">\n
-                            <p>{donnees[item]["type"]} {donnees[item]["caractere"]}  {donnees[item]["maj-min"]}</p>\n
-                            <p class="prblm">{donnees[item]["problème"]}</p>''')
-            g.write(f'''<p>{donnees[item]["commentaire"]}</p>\n''')
-            if donnees[item]["encodage"]:
-                g.write(f'''<p>Transcrire : {donnees[item]["encodage"]}</p>\n''')
-                # Ajouter l'entité avec &amp;
-                code = "" # Cette variable enfermera l'encodage en incluant les bons caractères d'échappement
-                for caractere in donnees[item]["encodage"]:
-                    if caractere != "&":
-                        code = code + caractere
-                    else:
-                        code = code + "&amp;"
-                g.write(f'''<p>Encodage : {code}</p>\n''')
-            g.write("</div>\n")
-            g.write("</article>\n")
-    
+    g.write("<header><h1>Table des caractères d'Arsenal 5070</h1></header>\n")
+    for chqType in ttTypes:
+        # Transformation du "type" en titre "h2" avec une maj. et au pluriel
+        transType = chqType.title()
+        transType = transType.split()
+        h2 = ""
+        for types in transType:
+            h2 += types + "s "
+        
+        # Ecriture du titre de la section
+        g.write("<section>\n")
+        g.write("<header><h2>" + h2 + "</h2><header>\n")
+        for index, item in enumerate(donnees):
+            if index != 0:
+                if chqType == donnees[item]["type"]:
+                    # Ecriture des articles
+                    g.write("<article>\n")
+                    
+                    # Div contenant les images
+                    g.write(f'''<div class="img"><img src="{item}" alt="{donnees[item]["caractere"]}" height="100"></div>\n''')
+                    
+                    # Div contenant le contenu des notices
+                    g.write(f'''<div class="descript">\n
+                                    <p>{donnees[item]["type"]} {donnees[item]["caractere"]}  {donnees[item]["maj-min"]}</p>\n
+                                    <p class="prblm">{donnees[item]["problème"]}</p>''')
+                    g.write(f'''<p>{donnees[item]["commentaire"]}</p>\n''')
+                    if donnees[item]["encodage"]:
+                        g.write(f'''<p>Transcrire : {donnees[item]["encodage"]}</p>\n''')
+                        # Ajouter l'entité avec &amp;
+                        code = "" # Cette variable enfermera l'encodage en incluant les bons caractères d'échappement
+                        for caractere in donnees[item]["encodage"]:
+                            if caractere != "&":
+                                code = code + caractere
+                            else:
+                                code = code + "&amp;"
+                        g.write(f'''<p>Encodage : {code}</p>\n''')
+                    g.write("</div>\n")
+                    g.write("</article>\n")
+        g.write("</section>\n")
     g.write("</body>\n")    
     g.write("</html>")
